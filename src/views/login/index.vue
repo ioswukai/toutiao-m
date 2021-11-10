e08b6fcec97accbc4ba7f02f2cc80a227b0b1fb4
吴凯 <ioswukai@163.com>
2021-11-10 09:21:01 +0800

    基础配置

---
+++ src/views/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      title="登录"
+    />
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {
+  },
+  mounted () {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+.container {
+}
+</style>
