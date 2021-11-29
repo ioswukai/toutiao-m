552e219fc8e9dd197b1f26e0e24c665dd3af4dc4
吴凯 <ioswukai@163.com>
2021-11-29 10:49:16 +0800

    引入 lodash 对联系建议请求，做防抖 调用

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -14,6 +14,7 @@
 
 <script>
 import { getSearchSuggestions } from '@/api/search'
+import debounce from 'lodash/debounce'
 
 export default {
   name: 'SearchSuggestion',
@@ -35,9 +36,13 @@ export default {
     searchText: {
       // 当searchText 发生改变的时候，就会调用handler 函数
       // 注意：handler 函数名是固定的
-      handler (val) {
+      // handler (val) {
+      //   this.loadSearchSuggestions(val)
+      // },
+      // 对请求调用做防抖优化 延时300ms
+      handler: debounce(function (val) {
         this.loadSearchSuggestions(val)
-      },
+      }, 300),
       // 使handler回调，在监听开始之后立即被调用
       // 以修复，第一次输入的内容，因组件未创建而无法被监听的bug
       immediate: true
